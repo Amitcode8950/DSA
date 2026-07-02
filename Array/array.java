f34d786d4a8326f29af86fe0eb01a0ce74032fe3
Amitcode8950 <amitkumar8950764059@gmail.com>
2026-07-02 14:14:41 +0530

    concept clear array

--- Array/array.java
+++ Array/array.java
@@ -1,8 +1,6 @@
-package Array;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
-
+import java.util.stream.IntStream;
 public class array {
 
     public static void main(String[] args) {
--- Problemsolving/siftarray.java
+++ Problemsolving/siftarray.java
@@ -1,17 +1,18 @@
 public class siftarray {
     static void arraysift(int[] arr){
         int n =arr.length;
-        int [] copyarray= new int[n];
-        for(int i = 0; i<arr.length; i++){
-          int a =arr[i];
-          copyarray[i+1] =a;
+        System.out.println(n);
+        int temp =arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
         }
-        for(int r: copyarray){
-            System.out.println(r);
-        }
+        arr[0]=temp;
     }
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6,7,8,9};
+        int[] arr={1,2,3,4,5};
         arraysift(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
+    }
 }
